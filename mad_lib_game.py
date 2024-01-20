aeec81403a935b03b2af76c9a3c25c40bf248f04
Kiyanna Gayle <kiyannagayle@Kiyannas-MacBook-Air.local>
2024-01-20 17:55:12 +0100

    mad lib game/input

---
+++ mad_lib_game.py
@@ -0,0 +1,7 @@
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+partner = input("Enter a partner's name: ")
+
+print("Roses are " + color + ".")
+print(plural_noun + " are blue")
+print("I love " + partner + "!")
