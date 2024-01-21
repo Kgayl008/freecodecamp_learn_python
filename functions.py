b27bd198de20ad4813a87c744f6e133158d3b373
Kiyanna Gayle <kiyannagayle@Kiyannas-MacBook-Air.local>
2024-01-21 16:24:04 +0100

    return statment

---
+++ functions.py
@@ -0,0 +1,13 @@
+
+def say_hi(name, age):
+    print("Hello " + name + " you are " + age)
+
+#how to "call" the code/ excuted
+# say_hi("Kiyanna", "23")
+# say_hi("Nikhil", "22")
+
+def cube(num):
+   return num*num*num
+print(cube(4))
+#return - python is going to excte the function, when it see the return statement
+# return is going to give a value back to whatever called the function
--- tuple.py
+++ tuple.py
@@ -1,4 +1,5 @@
 #tuple = immutable, prevent accidental addition, modification
 # , or removal of data, use less memory.
-
+# Lists are denoted by the SQUARE BRACKETS [] but tuples are denoted as PARENTHESIS ().
 coordinates = (4, 5)
+print(coordinates)
